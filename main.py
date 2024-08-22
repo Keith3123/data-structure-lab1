b20ac70ef09165ffc284f1e8ae0f26d0a1a6a730
Keith3123 <nicolemoralesberou35@gmail.com>
2024-08-22 11:49:36 +0800

    1st lab

---
+++ main.py
@@ -0,0 +1,17 @@
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+
+if first_num < second_num:
+    print(f"{first_num} is less than {second_num}")
+elif first_num > second_num:
+    print(f"{first_num} is greater than {second_num}")
+elif first_num == second_num:
+    print(f"{first_num} is equal to {second_num}")
+
+
+# name = input("Enter classmate name 1: ")
+# name_2 = input("Enter classmate name 2: ")
+# name_3 = input("Enter classmate name 3: ")
+#
+# print(f"Your classmates are: str{name}, {name_2}, & {name_3}")
+
